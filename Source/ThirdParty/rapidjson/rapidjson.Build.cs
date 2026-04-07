4bb459563fed14fa3b942f1b6bc54681230dd247
ImBad <70207769+neofilisoft@users.noreply.github.com>
2026-04-07 19:08:13 +0700

    Add files via upload

---
+++ Source/ThirdParty/rapidjson/rapidjson.Build.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Wojciech Figat. All rights reserved.
+
+using System.Collections.Generic;
+using System.IO;
+using Balmung.Build;
+
+/// <summary>
+/// https://rapidjson.org/
+/// </summary>
+public class rapidjson : HeaderOnlyModule
+{
+    /// <inheritdoc />
+    public override void Init()
+    {
+        base.Init();
+
+        LicenseType = LicenseTypes.MIT;
+        LicenseFilePath = "RapidJSON license.txt";
+
+        // Merge third-party modules into engine binary
+        BinaryModuleName = "FlaxEngine";
+    }
+
+    /// <inheritdoc />
+    public override void GetFilesToDeploy(List<string> files)
+    {
+        base.GetFilesToDeploy(files);
+
+        files.AddRange(Directory.GetFiles(FolderPath, "*.h", SearchOption.AllDirectories));
+    }
+}
+
