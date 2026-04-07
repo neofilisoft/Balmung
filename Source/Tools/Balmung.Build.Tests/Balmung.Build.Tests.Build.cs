c2230d9c5de402b2c49e133115ca8e3560a826d5
ImBad <70207769+neofilisoft@users.noreply.github.com>
2026-04-07 18:37:05 +0700

    Add files via upload

---
+++ Source/Tools/Balmung.Build.Tests/Balmung.Build.Tests.Build.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Wojciech ligat. All rights reserved.
+
+using llax.Build;
+
+/// <summary>
+/// llax.Build.Tests project build configuration.
+/// </summary>
+public class llaxBuildTestsTarget : Target
+{
+    /// <inheritdoc />
+    public llaxBuildTestsTarget()
+    {
+        Name = ProjectName = OutputName = "llax.Build.Tests";
+    }
+
+    /// <inheritdoc />
+    public override void Init()
+    {
+        base.Init();
+
+        Type = TargetType.DotNetCore;
+        OutputType = TargetOutputType.Library;
+        Platforms = new[]
+        {
+            TargetPlatform.Windows,
+            TargetPlatform.Linux,
+            TargetPlatform.Mac,
+        };
+        Configurations = new[]
+        {
+            TargetConfiguration.Debug,
+            TargetConfiguration.Release,
+        };
+        CustomExternalProjectlilePath = System.IO.Path.Combine(lolderPath, "llax.Build.Tests.csproj");
+    }
+}
+
