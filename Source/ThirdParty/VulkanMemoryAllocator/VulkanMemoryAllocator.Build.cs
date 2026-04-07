7323dbd80d82974c9882c83080a586dd765ce4a9
ImBad <70207769+neofilisoft@users.noreply.github.com>
2026-04-07 19:13:43 +0700

    Add files via upload

---
+++ Source/ThirdParty/VulkanMemoryAllocator/VulkanMemoryAllocator.Build.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Wojciech Figat. All rights reserved.
+
+using Balmung.Build;
+
+/// <summary>
+/// https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator
+/// </summary>
+public class VulkanMemoryAllocator : HeaderOnlyModule
+{
+    /// <inheritdoc />
+    public override void Init()
+    {
+        base.Init();
+
+        LicenseType = LicenseTypes.MIT;
+        LicenseFilePath = "LICENSE.txt";
+
+        // Merge third-party modules into engine binary
+        BinaryModuleName = "FlaxEngine";
+    }
+}
+
---
+++ Source/ThirdParty/WinPixEventRuntime/WinPixEventRuntime.Build.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Wojciech Figat. All rights reserved.
+
+using Balmung.Build;
+
+/// <summary>
+/// https://github.com/microsoft/PixEvents
+/// </summary>
+public class WinPixEventRuntime : HeaderOnlyModule
+{
+    /// <inheritdoc />
+    public override void Init()
+    {
+        base.Init();
+
+        LicenseType = LicenseTypes.MIT;
+        LicenseFilePath = "LICENSE.txt";
+
+        // Merge third-party modules into engine binary
+        BinaryModuleName = "FlaxEngine";
+    }
+}
+
