83d57a9652d1e0df6e34c058ef23be74a02a7086
ImBad <70207769+neofilisoft@users.noreply.github.com>
2026-04-07 19:53:35 +0700

    Create GPUAdapterNull.h

---
+++ Source/Engine/GraphicsDevice/Null/GPUAdapterNull.h
@@ -0,0 +1,39 @@
+// Copyright (c) Neofilisoft. All rights reserved.
+
+#pragma once
+
+#if GRAPHICS_API_NULL
+
+#include "Engine/Graphics/GPUAdapter.h"
+
+/// <summary>
+/// Graphics Device adapter implementation for Null backend.
+/// </summary>
+class GPUAdapterNull : public GPUAdapter
+{
+public:
+    // [GPUAdapter]
+    bool IsValid() const override
+    {
+        return true;
+    }
+    void* GetNativePtr() const override
+    {
+        return nullptr;
+    }
+    uint32 GetVendorId() const override
+    {
+        return 0;
+    }
+    String GetDescription() const override
+    {
+        return TEXT("Null");
+    }
+    Version GetDriverVersion() const override
+    {
+        return Version(0, 0);
+    }
+};
+
+#endif
+
