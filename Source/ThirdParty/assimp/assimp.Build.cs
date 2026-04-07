0385296b60d88e64070bcd02de01827ac96282f9
ImBad <70207769+neofilisoft@users.noreply.github.com>
2026-04-07 18:45:57 +0700

    Add files via upload

---
+++ Source/ThirdParty/assimp/assimp.Build.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Wojciech Figat. All rights reserved.
+
+using System.IO;
+using Balmung.Build;
+using Balmung.Build.NativeCpp;
+
+/// <summary>
+/// https://www.assimp.org/
+/// </summary>
+public class assimp : DepsModule
+{
+    /// <inheritdoc />
+    public override void Init()
+    {
+        base.Init();
+
+        LicenseType = LicenseTypes.Custom;
+        LicenseFilePath = "LICENSE";
+
+        // Merge third-party modules into engine binary
+        BinaryModuleName = "FlaxEngine";
+    }
+
+    /// <inheritdoc />
+    public override void Setup(BuildOptions options)
+    {
+        base.Setup(options);
+
+        var depsRoot = options.DepsFolder;
+        switch (options.Platform.Target)
+        {
+        case TargetPlatform.Windows:
+            options.OutputFiles.Add(Path.Combine(depsRoot, "assimp-vc140-md.lib"));
+            options.DependencyFiles.Add(Path.Combine(depsRoot, "assimp-vc140-md.dll"));
+            options.DelayLoadLibraries.Add("assimp-vc140-md.dll");
+            break;
+        case TargetPlatform.Linux:
+        case TargetPlatform.Mac:
+            options.OutputFiles.Add(Path.Combine(depsRoot, "libassimp.a"));
+            break;
+        default: throw new InvalidPlatformException(options.Platform.Target);
+        }
+    }
+}
+
