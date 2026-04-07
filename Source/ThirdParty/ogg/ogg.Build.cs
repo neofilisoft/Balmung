1137abbbadf023115a99eca8b5c1aa2b5f5feac8
ImBad <70207769+neofilisoft@users.noreply.github.com>
2026-04-07 19:07:17 +0700

    Add files via upload

---
+++ Source/ThirdParty/ogg/ogg.Build.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Wojciech Figat. All rights reserved.
+
+using System.IO;
+using Balmung.Build;
+using Balmung.Build.NativeCpp;
+
+/// <summary>
+/// https://github.com/xiph/ogg
+/// </summary>
+public class ogg : DepsModule
+{
+    /// <inheritdoc />
+    public override void Init()
+    {
+        base.Init();
+
+        LicenseType = LicenseTypes.Custom;
+        LicenseFilePath = "COPYING";
+
+        // Merge third-party modules into engine binary
+        BinaryModuleName = "FlaxEngine";
+    }
+
+    /// <inheritdoc />
+    public override void Setup(BuildOptions options)
+    {
+        base.Setup(options);
+
+        var depsRoot = options.DepsFolder;
+        switch (options.Platform.Target)
+        {
+        case TargetPlatform.Windows:
+        case TargetPlatform.UWP:
+        case TargetPlatform.XboxOne:
+        case TargetPlatform.XboxScarlett:
+            options.OutputFiles.Add(Path.Combine(depsRoot, "libogg_static.lib"));
+            options.OptionalDependencyFiles.Add(Path.Combine(depsRoot, "libogg_static.pdb"));
+            break;
+        case TargetPlatform.Linux:
+        case TargetPlatform.PS4:
+        case TargetPlatform.PS5:
+        case TargetPlatform.Android:
+        case TargetPlatform.Switch:
+        case TargetPlatform.Mac:
+        case TargetPlatform.iOS:
+            options.OutputFiles.Add(Path.Combine(depsRoot, "libogg.a"));
+            break;
+        default: throw new InvalidPlatformException(options.Platform.Target);
+        }
+    }
+}
+
---
+++ Source/ThirdParty/pugixml/pugixml.Build.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Wojciech Figat. All rights reserved.
+
+using Balmung.Build;
+
+/// <summary>
+/// https://pugixml.org/
+/// </summary>
+public class pugixml : ThirdPartyModule
+{
+    /// <inheritdoc />
+    public override void Init()
+    {
+        base.Init();
+
+        LicenseType = LicenseTypes.MIT;
+        LicenseFilePath = "pugixml license.txt";
+
+        // Merge third-party modules into engine binary
+        BinaryModuleName = "FlaxEngine";
+    }
+}
+
