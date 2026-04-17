bce78d50525918a316c6b71555795ebd0f58cf90
ImBad <70207769+neofilisoft@users.noreply.github.com>
2026-04-17 21:36:22 +0700

    Add files via upload

---
+++ Source/Editor/Options/Editor.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Neofilisoft. All rights reserved.
+
+using System.Text;
+using BalmungEditor.CustomEditors;
+using BalmungEditor.CustomEditors.Editors;
+using FlaxEngine;
+using BalmungEngine.GUI;
+using BalmungEngine.Json;
+
+namespace BalmungEditor.Options
+{
+    /// <summary>
+    /// Editor options editor.
+    /// </summary>
+    public class Editor<T> : GenericEditor
+    where T : new()
+    {
+        /// <inheritdoc />
+        public override void Initialize(LayoutElementsContainer layout)
+        {
+            base.Initialize(layout);
+
+            // Reset options button
+            layout.Space(30);
+            var panel = layout.Space(30);
+            var resetButton = new Button(4, 4, 100)
+            {
+                Text = "Reset",
+                Parent = panel.ContainerControl
+            };
+            resetButton.Clicked += OnResetButtonClicked;
+        }
+
+        private void OnResetButtonClicked()
+        {
+            var editorClassName = typeof(T).Name;
+            
+            var editorName = new StringBuilder();
+            editorName.Append(editorClassName[0]);
+            for (var i = 1; i < editorClassName.Length; i++)
+            {
+                // Whenever there is an uppercase letter, add a space to make it more pretty for the end user
+                if (char.IsUpper(editorClassName[i]))
+                {
+                    editorName.Append(' ');
+                }
+                editorName.Append(editorClassName[i]);
+            }
+            
+            var result = MessageBox.Show($"Are you sure you want to reset \"{editorName}\" to default values?", 
+                                         "Reset values?",
+                                         MessageBoxButtons.YesNo
+                                         );
+            
+            if (result == DialogResult.Yes || result == DialogResult.OK)
+            {
+                var obj = new T();
+                var str = JsonSerializer.Serialize(obj);
+                JsonSerializer.Deserialize(Values[0], str);
+                SetValue(Values[0]);
+                Refresh();
+            }
+        }
+    }
+}
+
+
---
+++ Source/Editor/Options/ThemeOptions.cs
@@ -0,0 +1,111 @@
+﻿// Copyright (c) Neofilisoft. All rights reserved.
+
+using System.Collections.Generic;
+using BalmungEditor.CustomEditors;
+using BalmungEditor.CustomEditors.Elements;
+using BalmungEditor.GUI;
+using FlaxEngine;
+using BalmungEngine.GUI;
+
+namespace BalmungEditor.Options
+{
+    /// <summary>
+    /// Theme options data container object.
+    /// </summary>
+    [CustomEditor(typeof(ThemeOptionsEditor))]
+    public sealed class ThemeOptions
+    {
+        internal const string DefaultName = "Default";
+        internal const string LightDefault = "LightDefault";
+
+        internal class ThemeOptionsEditor : Editor<ThemeOptions>
+        {
+            private LabelElement _infoLabel;
+
+            /// <inheritdoc />
+            public override void Initialize(LayoutElementsContainer layout)
+            {
+                _infoLabel = layout.Label("Editor restart is required to apply style changes.", TextAlignment.Center);
+                _infoLabel.Label.Visible = false;
+
+                base.Initialize(layout);
+            }
+
+            /// <inheritdoc />
+            protected override void Deinitialize()
+            {
+                _infoLabel = null;
+
+                base.Deinitialize();
+            }
+
+            /// <inheritdoc />
+            protected override bool OnDirty(CustomEditor editor, object value, object token = null)
+            {
+                _infoLabel.Label.Visible = true;
+                return base.OnDirty(editor, value, token);
+            }
+        }
+
+        internal class StyleOptionsEditor : CustomEditor
+        {
+            private ComboBoxElement _combobox;
+
+            /// <inheritdoc />
+            public override DisplayStyle Style => DisplayStyle.Inline;
+
+            /// <inheritdoc />
+            public override void Initialize(LayoutElementsContainer layout)
+            {
+                _combobox = layout.ComboBox();
+                ReloadOptions(_combobox.ComboBox);
+                _combobox.ComboBox.SelectedIndexChanged += OnComboBoxSelectedIndexChanged;
+                _combobox.ComboBox.PopupShowing += ReloadOptions;
+            }
+
+            private void ReloadOptions(ComboBox obj)
+            {
+                var themeOptions = (ThemeOptions)ParentEditor.Values[0];
+                var options = new string[themeOptions.Styles.Count + 2];
+                options[0] = DefaultName;
+                options[1] = LightDefault;
+
+                int i = 0;
+                foreach (var styleName in themeOptions.Styles.Keys)
+                {
+                    options[i + 2] = styleName;
+                    i++;
+                }
+                _combobox.ComboBox.SetItems(options);
+                _combobox.ComboBox.SelectedItem = (string)Values[0];
+            }
+
+            private void OnComboBoxSelectedIndexChanged(ComboBox combobox)
+            {
+                SetValue(combobox.SelectedItem);
+            }
+
+            /// <inheritdoc />
+            public override void Refresh()
+            {
+                _combobox.ComboBox.SelectedItem = (string)Values[0];
+
+                base.Refresh();
+            }
+        }
+
+        /// <summary>
+        /// Currently selected style.
+        /// </summary>
+        [CustomEditor(typeof(StyleOptionsEditor))]
+        [Tooltip("Restart Editor to apply style")]
+        public string SelectedStyle = "Default";
+
+        /// <summary>
+        /// All available styles.
+        /// </summary>
+        public Dictionary<string, Style> Styles { get; set; } = new Dictionary<string, Style>();
+    }
+}
+
+
