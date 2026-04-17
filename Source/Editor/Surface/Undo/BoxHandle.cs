5feaf8af6e4125a01089f8984a79fd9a888963de
ImBad <70207769+neofilisoft@users.noreply.github.com>
2026-04-17 22:01:14 +0700

    Add files via upload

---
+++ Source/Editor/Surface/Undo/BoxHandle.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (c) Neofilisoft. All rights reserved.
+
+using System;
+using BalmungEditor.Surface.Elements;
+using FlaxEngine;
+
+namespace BalmungEditor.Surface.Undo
+{
+    /// <summary>
+    /// The helper structure for Surface node box handle.
+    /// </summary>
+    [HideInEditor]
+    public struct BoxHandle : IEquatable<BoxHandle>
+    {
+        private readonly uint _nodeId;
+        private readonly int _boxId;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BoxHandle"/> struct.
+        /// </summary>
+        /// <param name="nodeId">The node identifier.</param>
+        /// <param name="boxId">The box identifier.</param>
+        public BoxHandle(uint nodeId, int boxId)
+        {
+            _nodeId = nodeId;
+            _boxId = boxId;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BoxHandle"/> struct.
+        /// </summary>
+        /// <param name="box">The box.</param>
+        public BoxHandle(Box box)
+        {
+            _nodeId = box.ParentNode.ID;
+            _boxId = box.ID;
+        }
+
+        /// <summary>
+        /// Gets the box.
+        /// </summary>
+        /// <param name="context">The Surface context.</param>
+        /// <returns>The restored box.</returns>
+        public Box Get(VisjectSurfaceContext context)
+        {
+            var node = context.FindNode(_nodeId);
+            if (node == null)
+                throw new Exception("Missing node.");
+            var box = node.GetBox(_boxId);
+            if (box == null)
+                throw new Exception("Missing box.");
+            return box;
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            return obj is BoxHandle handle && Equals(handle);
+        }
+
+        /// <inheritdoc />
+        public bool Equals(BoxHandle other)
+        {
+            return _nodeId == other._nodeId &&
+                   _boxId == other._boxId;
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (_nodeId.GetHashCode() * 397) ^ _boxId.GetHashCode();
+            }
+        }
+    }
+}
+
+
---
+++ Source/Editor/Surface/Undo/MoveNodesAction.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Neofilisoft. All rights reserved.
+
+using System;
+using FlaxEngine;
+
+namespace BalmungEditor.Surface.Undo
+{
+    /// <summary>
+    /// Move Visject Surface nodes undo action.
+    /// </summary>
+    /// <seealso cref="BalmungEditor.IUndoAction" />
+    class MoveNodesAction : IUndoAction
+    {
+        private VisjectSurface _surface;
+        private ContextHandle _context;
+        private uint[] _nodeIds;
+        private readonly Float2 _locationDelta;
+
+        public MoveNodesAction(VisjectSurfaceContext context, uint[] nodeIds, Float2 locationDelta)
+        {
+            _surface = context.Surface;
+            _context = new ContextHandle(context);
+            _nodeIds = nodeIds;
+            _locationDelta = locationDelta;
+        }
+
+        /// <inheritdoc />
+        public string ActionString => "Move nodes";
+
+        /// <inheritdoc />
+        public void Do()
+        {
+            Apply(_locationDelta);
+        }
+
+        /// <inheritdoc />
+        public void Undo()
+        {
+            Apply(-_locationDelta);
+        }
+
+        private void Apply(Float2 delta)
+        {
+            var context = _context.Get(_surface);
+            foreach (var nodeId in _nodeIds)
+            {
+                var node = context.FindNode(nodeId);
+                if (node == null)
+                    throw new Exception("Missing node.");
+
+                node.Location += delta;
+            }
+
+            context.MarkAsModified(false);
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            _surface = null;
+            _nodeIds = null;
+        }
+    }
+}
+
+
